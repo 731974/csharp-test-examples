a9ff7ff42a2389ab1908ccdf4ebbe317210b310a
Mathijs <92179923+ThijsKlein@users.noreply.github.com>
2026-03-13 15:10:16 +0100

    Tja?

---
+++ TestingExamples/Task3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Linq;
+
+namespace Task3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            program.Start();
+        }
+
+        public void Start()
+        {
+            Reverse();
+        }
+
+        public int Reverse(int x = -321)
+        {
+            string str = x.ToString();
+            string add = "";
+
+            if (str[0] == '-')
+            {
+                add = "-";
+                str = str.Substring(1); 
+            }
+
+            char[] chars = str.ToCharArray();
+            Array.Reverse(chars);
+            string reversed = new string(chars);
+
+            return int.Parse(add + reversed);
+        }
+
+
+    }
+}
